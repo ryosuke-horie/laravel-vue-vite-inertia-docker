1031e2f0ec96f9f651268f07cb57d5f947dcdc70
fsworks\r-horie <r-horie@mamiya-its.co.jp>
2023-09-21 15:19:54 +0900

    :+1: 開発用に動作できる状態に修正

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: '192.168.100.230',
+        },
+        watch: {
+            usePolling: true,
+        },
+        port:5173
+    }
 });
