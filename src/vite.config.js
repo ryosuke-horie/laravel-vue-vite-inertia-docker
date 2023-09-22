50de39ec73f1a53961e48435a175cfb4514b316a
fsworks\r-horie <r-horie@mamiya-its.co.jp>
2023-09-22 11:40:54 +0900

    :books: document修正

--- src/vite.config.js
+++ src/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         hmr: {
-            host: '192.168.100.230',
+            host: 'xxx.xxx.xxx.xxx',
         },
         watch: {
             usePolling: true,
